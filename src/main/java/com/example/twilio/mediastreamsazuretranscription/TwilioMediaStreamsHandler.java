911b8873bb9146b100d3e72dee5a0066ed9411e7
Matthew Gilliard <matthew@gilliard.lol>
2021-05-14 16:34:33 +0100

    Adds the Azure Speech client
    Includes config and dependencies, and hooks the Azure client into our websocket handler.
    
    The demo app is now complete.

--- src/main/java/com/example/twilio/mediastreamsazuretranscription/TwilioMediaStreamsHandler.java
+++ src/main/java/com/example/twilio/mediastreamsazuretranscription/TwilioMediaStreamsHandler.java
@@ -1,24 +1,50 @@
 package com.example.twilio.mediastreamsazuretranscription;
 
+import com.example.twilio.mediastreamsazuretranscription.azure.AzureSTTService;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.web.socket.CloseStatus;
 import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.AbstractWebSocketHandler;
 
+import java.util.Base64;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
 public class TwilioMediaStreamsHandler extends AbstractWebSocketHandler {
+
+    private final Map<WebSocketSession, AzureSTTService> sessions = new ConcurrentHashMap<>();
+
+    private final ObjectMapper jsonMapper = new ObjectMapper();
+    private final Base64.Decoder base64Decoder = Base64.getDecoder();;
 
     @Override
     public void afterConnectionEstablished(WebSocketSession session) {
         System.out.println("Connection Established");
+        sessions.put(session, new AzureSTTService(System.out::println));
     }
 
     @Override
-    protected void handleTextMessage(WebSocketSession session, TextMessage message) {
-        System.out.println("Message");
+    protected void handleTextMessage(WebSocketSession session, TextMessage message) throws JsonProcessingException {
+
+        JsonNode messageNode = jsonMapper.readTree(message.getPayload());
+
+        String base64EncodedAudio = messageNode.path("media").path("payload").asText();
+
+        if (base64EncodedAudio.length() > 0){
+            // not every message contains audio data
+            byte[] data = base64Decoder.decode(base64EncodedAudio);
+            sessions.get(session).accept(data);
+        }
+
     }
 
     @Override
     public void afterConnectionClosed(WebSocketSession session, CloseStatus status) {
         System.out.println("Connection Closed");
+        sessions.get(session).close();
+        sessions.remove(session);
     }
 }
